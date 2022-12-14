3a7b80a28fd6904aeda159f3688fd258ebdb8859
Anastasia Sharubina <as.sharubina@thescholasticnetwork.com>
2022-12-14 14:17:28 +0700

    Update webpack.config.js

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -328,7 +328,9 @@ module.exports = function (webpackEnv) {
                     'react-dom$': 'react-dom/profiling',
                     'scheduler/tracing': 'scheduler/tracing-profiling',
                 }),
-                ...(modules.webpackAliases || {})
+                ...(modules.webpackAliases || {}),
+                react: path.resolve('./node_modules/react'),
+                'react-router-dom': path.resolve('./node_modules/react-router-dom'),
             },
             plugins: [
                 // Prevents users from importing files from outside of src/ (or node_modules/).
