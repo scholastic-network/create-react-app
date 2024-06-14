ebb9196324b9e0300264cbe4974a3225746af434
Anastasia Sharubina <as.sharubina@thescholasticnetwork.com>
2024-06-14 14:19:04 +0700

    Update webpack.config.js

--- packages/react-scripts/config/webpack.config.js
+++ packages/react-scripts/config/webpack.config.js
@@ -371,15 +371,6 @@ module.exports = function(webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
-            {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-              type: 'asset',
-              parser: {
-                dataUrlCondition: {
-                  maxSize: imageInlineSizeLimit
-                }
-              }
-            },
             // TODO: Merge this config once `image/avif` is in the mime-db
             // https://github.com/jshttp/mime-db
             {
@@ -619,11 +610,12 @@ module.exports = function(webpackEnv) {
               )
             },
             {
-              test: /\.(woff(2)?|ttf|eot|svg)$/,
+              test: /\.(ttf|woff|woff2)$/,
+              exclude: /node_modules/,
               type: 'asset/resource',
               generator: {
-                publicPath: '/res/fonts/',  // Ensure this matches the actual path on your server
-              },
+                filename: 'static/media/fonts/[name][ext]'
+              }
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
@@ -635,7 +627,7 @@ module.exports = function(webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/, /\.(ttf|woff|woff2)$/],
+              exclude: [/^$/, /\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
               type: 'asset/resource'
             }
             // ** STOP ** Are you adding a new loader?
